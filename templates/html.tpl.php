03aa0de03a72e3cc20554c8c3517a0b520e2a692
Thomas Thune Hansen <tth@devel.(none)>
2013-10-02 11:42:43 +0200

    update viewport meta

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -6,8 +6,6 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
